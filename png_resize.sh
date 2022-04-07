f286bf73983c5acd003ad75aa28f44b40ad0dace
CubanJaco <46829979+CubanJaco@users.noreply.github.com>
2022-04-07 10:09:53 -0400

    First commit with script

---
+++ png_resize.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+image=$1
+start_resolution=$2
+start_type=0
+
+width=$(identify -format '%w' $image)
+height=$(identify -format '%h' $image)
+
+wbase=0.0
+hbase=0.0
+
+if [ $start_resolution = "xxxhdpi" ]; then
+	wbase=$width
+	hbase=$height
+	start_type=6
+fi
+
+if [ $start_resolution = "xxhdpi" ]; then
+	wbase=$( expr $width / 0.75 )
+	hbase=$( expr $height / 0.75 )
+	start_type=5
+fi
+
+if [ $start_resolution = "xhdpi" ]; then
+	wbase=$( expr $width / 0.5 )
+	hbase=$( expr $height / 0.5 )
+	start_type=4
+fi
+
+if [ $start_resolution = "hdpi" ]; then
+	wbase=$( expr $width / 0.375 )
+	hbase=$( expr $height / 0.375 )
+	start_type=3
+fi
+
+if [ $start_resolution = "mdpi" ]; then
+	wbase=$( expr $width / 0.25 )
+	hbase=$( expr $height / 0.25 )
+	start_type=2
+fi
+
+if [ $width = 0 ]; then
+	exit 1
+fi
+
+if [ $height = 0 ]; then
+	exit 1
+fi
+
+percent_xxxhdpi=100%
+percent_xxhdpi=75%
+percent_xhdpi=50%
+percent_hdpi=37%
+percent_mdpi=25%
+percent_ldpi=19%
+
+if [ "$start_type" -eq 5 ]; then
+	percent_xxxhdpi=100%
+	percent_xxhdpi=100%
+	percent_xhdpi=67%
+	percent_hdpi=50%
+	percent_mdpi=33%
+	percent_ldpi=25%
+fi
+
+if [ "$start_type" -eq 4 ]; then
+	percent_xxxhdpi=100%
+	percent_xxhdpi=100%
+	percent_xhdpi=100%
+	percent_hdpi=75%
+	percent_mdpi=50%
+	percent_ldpi=37%
+fi
+
+if [ "$start_type" -eq 3 ]; then
+	percent_xxxhdpi=100%
+	percent_xxhdpi=100%
+	percent_xhdpi=100%
+	percent_hdpi=100%
+	percent_mdpi=67%
+	percent_ldpi=50%
+fi
+
+if [ "$start_type" -eq 2 ]; then
+	percent_xxxhdpi=100%
+	percent_xxhdpi=100%
+	percent_xhdpi=100%
+	percent_hdpi=100%
+	percent_mdpi=100%
+	percent_ldpi=75%
+fi
+
+if [ "$start_type" -eq 1 ]; then
+	percent_xxxhdpi=100%
+	percent_xxhdpi=100%
+	percent_xhdpi=100%
+	percent_hdpi=100%
+	percent_mdpi=100%
+	percent_ldpi=100%
+fi
+
+if [ -d "./drawable-xxxhdpi" ]; then
+	echo "Not need to create directory ./drawable-xxxhdpi because already exist"
+else
+	echo "Creating directory ./drawable-xxxhdpi."
+	mkdir drawable-xxxhdpi
+fi
+
+echo "Resizing image for xxxhdpi resolution"
+convert -resize $percent_xxxhdpi $image drawable-xxxhdpi/$image
+echo " "
+
+if [ -d "./drawable-xxhdpi" ]; then
+	echo "Not need to create directory ./drawable-xxhdpi because already exist"
+else
+	echo "Creating directory ./drawable-xxhdpi."
+	mkdir drawable-xxhdpi
+fi
+
+echo "Resizing image for xxhdpi resolution"
+convert -resize $percent_xxhdpi $image drawable-xxhdpi/$image
+echo " "
+
+if [ -d "./drawable-xhdpi" ]; then
+	echo "Not need to create directory ./drawable-xhdpi because already exist"
+else
+	echo "Creating directory ./drawable-xhdpi."
+	mkdir drawable-xhdpi
+fi
+
+echo "Resizing image for xhdpi resolution"
+convert -resize $percent_xhdpi $image drawable-xhdpi/$image
+echo " "
+
+if [ -d "./drawable-hdpi" ]; then
+	echo "Not need to create directory ./drawable-hdpi because already exist"
+else
+	echo "Creating directory ./drawable-hdpi."
+	mkdir drawable-hdpi
+fi
+
+echo "Resizing image for hdpi resolution"
+convert -resize $percent_hdpi $image drawable-hdpi/$image
+echo " "
+
+if [ -d "./drawable-mdpi" ]; then
+	echo "Not need to create directory ./drawable-mdpi because already exist"
+else
+	echo "Creating directory ./drawable-mdpi."
+	mkdir drawable-mdpi
+fi
+
+echo "Resizing image for mdpi resolution"
+convert -resize $percent_mdpi $image drawable-mdpi/$image
+echo " "
+
+if [ -d "./drawable-ldpi" ]; then
+	echo "Not need to create directory ./drawable-ldpi because already exist"
+else
+	echo "Creating directory ./drawable-ldpi."
+	mkdir drawable-ldpi
+fi
+
+echo "Resizing image for ldpi resolution"
+convert -resize $percent_ldpi $image drawable-ldpi/$image
